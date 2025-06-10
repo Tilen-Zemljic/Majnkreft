53d87a1db1effaad631ac76c3fa06479c5090e36
Tilen-Zemljic <166599775+Tilen-Zemljic@users.noreply.github.com>
2025-06-10 20:28:33 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,18 @@
+# Moj Minecraft Projekt
+
+To je preprosta večstranska spletna stran, ki predstavlja igro Minecraft.
+
+## Vsebina
+- `index.html` – uvod in opis igre
+- `gallery.html` – galerija slik
+- `contact.html` – kontaktni obrazec
+
+## Tehnične podrobnosti
+- HTML + CSS + JavaScript
+- Responsive dizajn
+- Interaktivna galerija in validacija obrazca
+- Objavljeno preko GitHub Pages
+
+## Objava
+Repozitorij: https://github.com/tvojeUporabniskoIme/minecraft-projekt  
+Spletna stran: https://tvojeUporabniskoIme.github.io/minecraft-projekt/
---
+++ script.js
@@ -0,0 +1,28 @@
+function prikaziSporocilo() {
+    alert("Rdečega gumba se ne pritiska!");
+  }
+
+  function spremeniBarvo() {  
+      const r = Math.floor(Math.random() * 256);
+      const g = Math.floor(Math.random() * 256);
+      const b = Math.floor(Math.random() * 256);
+      document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+  }
+  function preveriObrazec() {
+    let ime = document.getElementById("ime").value.trim();
+    let email = document.getElementById("email").value.trim();
+
+    if (ime === "" || email === "") {
+      alert("Prosim, izpolnite vsa polja obrazca.");
+      return false;
+    }
+
+    alert("Obrazec je uspešno poslan!");
+    return true;
+  }
+  function showImage(imgElement) {
+    const previewSection = document.getElementById('preview');
+    const previewImg = document.getElementById('previewImg');
+    previewImg.src = imgElement.src;
+    previewSection.classList.remove('hidden');
+  }
